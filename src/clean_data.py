c9054298bbc1b73985b0fe0508e763c2c791c42d
daniloldn <112765545+daniloldn@users.noreply.github.com>
2026-02-23 18:54:00 +0000

    host feautes

--- src/clean_data.py
+++ src/clean_data.py
@@ -9,9 +9,9 @@ def clean_data() -> pd.DataFrame:
 
     #dropping columns that are not relevant for our analysis
     drop_cols = ["listing_url", "name",
-              "neighborhood_overview","description","source","picture_url", "host_url", "host_id",
+              "neighborhood_overview","source","picture_url", "host_url", "host_id",
                 "host_thumbnail_url", "host_picture_url", "estimated_occupancy_l365d", "estimated_revenue_l365d",
-                 "host_about", "host_location" ,"host_name", "neighbourhood_cleansed", "neighbourhood",
+                  "host_location" ,"host_name", "neighbourhood_cleansed", "neighbourhood",
                   "availability_30", "availability_60", "availability_90", "availability_365", "availability_eoy",
                    "minimum_minimum_nights", "maximum_minimum_nights", "minimum_maximum_nights", "maximum_maximum_nights",
                    "minimum_nights_avg_ntm", "maximum_nights_avg_ntm", "calendar_updated", "calendar_last_scraped",
@@ -20,13 +20,9 @@ def clean_data() -> pd.DataFrame:
                    "license"]
     
     df.drop(columns=drop_cols, inplace=True)
-    #dropping rows with missing values
-    df.dropna(inplace=True)
 
     # converting data types to correct types
     df["host_response_time"] = df["host_response_time"].astype("category")
-    df["host_is_superhost"] = df["host_is_superhost"].map({"t": 1, "f": 0}).astype("int8")
-    df["host_identity_verified"] = df["host_identity_verified"].map({"t": 1, "f": 0}).astype("int8")
     df["last_scraped"] = pd.to_datetime(df["last_scraped"])
     df["host_since"] = pd.to_datetime(df["host_since"])
     df["host_response_rate"] = df["host_response_rate"].str.rstrip("%").astype("float") / 100
@@ -34,7 +30,6 @@ def clean_data() -> pd.DataFrame:
     df["first_review"] = pd.to_datetime(df["first_review"])
     df["last_review"] = pd.to_datetime(df["last_review"])
     df["instant_bookable"] = df["instant_bookable"].map({"t": 1, "f": 0}).astype("int8")
-    df["host_has_profile_pic"] = df["host_has_profile_pic"].map({"t": 1, "f": 0}).astype("int8")
     df["host_verifications"] = df["host_verifications"].apply(lambda x: len(x.strip("[]").split(", ")) if pd.notnull(x) else 0).astype("int8")
     df["host_acceptance_rate"] = df["host_acceptance_rate"].str.rstrip("%").astype("float") / 100
 
--- src/feature_eng.py
+++ src/feature_eng.py
@@ -8,4 +8,23 @@ def feature_eng() -> pd.DataFrame:
 
     df = load_processed()
 
+
+    #host features
+    df["experience"] = df["last_scraped"].dt.year - df["host_since"].dt.year
+    df["desc_dummy"] = df["description"].apply(lambda x: 0 if pd.isnull(x) else 1)
+    df["about_dummy"] = df["host_about"].apply(lambda x: 0 if pd.isnull(x) else 1)
+
+
+    #converting categorical variables
+    df["host_is_superhost"] = df["host_is_superhost"].map({"t": 1, "f": 0}).astype("int8")
+    df["host_identity_verified"] = df["host_identity_verified"].map({"t": 1, "f": 0}).astype("int8")
+    df["host_has_profile_pic"] = df["host_has_profile_pic"].map({"t": 1, "f": 0}).astype("int8")
+    
+    
+    #dropping rows with missing values
+    df.dropna(inplace=True)
+
+    #dropping columns no longer needed
+    df.drop(columns=["last_scraped", "host_since", "description", "host_about"], inplace=True)
+
     return None
